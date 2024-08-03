2e16b3019b0ecb89ac7c92dd45e54784bf9fbb34
Jean C. <j-c_f@outlook.com>
2024-08-03 18:17:32 -0500

    UPD: firebase basic config to use App distribution

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,17 +1,23 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kasanipedido/app/app.dart';
 import 'package:kasanipedido/bootstrap.dart';
 import 'package:kasanipedido/data/services/order_booking/order_service.dart';
 import 'package:kasanipedido/domain/repository/order_booking/order_booking_api_impl.dart';
 import 'package:kasanipedido/domain/repository/order_booking/order_booking_repository.dart';
+import 'package:kasanipedido/firebase_options.dart';
 import 'package:products_api_impl/products_api_impl.dart';
 import 'package:shopping_cart_repository/shopping_cart_repository.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-    final dioInstance = Dio();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final dioInstance = Dio();
 
   final productService = ProductService(dio: dioInstance);
   final productsApi = ProductsApiImpl(productService: productService);
@@ -20,8 +26,7 @@ Future<void> main() async {
       ShoppingCartRepository(productsApi: productsApi);
 
   final orderService = OrderService(dio: dioInstance);
-  final orderBookingApi =
-      OrderBookingApiImpl(orderService: orderService);
+  final orderBookingApi = OrderBookingApiImpl(orderService: orderService);
 
   final orderBookingRepository =
       OrderRepository(orderBookingApi: orderBookingApi);
